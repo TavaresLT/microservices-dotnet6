d77ef00071b19b4acf6387518f544e500f504e80
Lucas Tavares <lucastvspvh90@gmail.com>
2023-06-06 08:04:51 -0400

    criando a classe product e adicionando migrations ao banco de dados

--- GeekShooping/GeekShopping.ProductAPI/Program.cs
+++ GeekShooping/GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,6 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace GeekShopping.ProductAPI
 {
     public class Program
@@ -7,6 +10,11 @@ namespace GeekShopping.ProductAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+            builder.Services.AddDbContext<MySQLContext>(options => 
+            options.UseMySql(connection, new MySqlServerVersion(
+                new Version(8, 0, 5))));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
